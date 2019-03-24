6d331da2420c61ef5bc749d6876090a6a0eb2df7
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2019-03-24 13:03:36 -0400

    Compound interest program

--- statsim/mom.py
+++ statsim/mom.py
@@ -8,10 +8,13 @@ Repeat until no more money, then count as LOSE
 """
 
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-n_trials = 1000000
+n_trials = 100000
 n_wins = 0
 n_money = []
+n_ties = 0
 
 for i in range(n_trials):
     money = 4
@@ -23,6 +26,9 @@ for i in range(n_trials):
             bet *= 2 # double the next bet
             #print(f'won first bet, at {money}, bet is {bet}')
             if (not np.random.choice((0, 1))): # won twice in a row
+                if money == 4: # break even
+                    n_ties += 1
+                    break
                 money += bet # add money
                 n_wins += 1 # log win
                 n_money.append(money) # log cash at end of game
@@ -38,3 +44,7 @@ for i in range(n_trials):
 
 print(n_wins/n_trials*100)
 print(np.mean(n_money))
+print(n_ties/n_trials*100)
+print((1-(n_wins+n_ties)/n_trials)*100)
+#sns.distplot(n_money)
+#plt.show()
