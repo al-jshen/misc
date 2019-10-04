187c66cce123b3b1521499e772aac180a43ea6f7
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2019-10-04 16:07:17 -0400

    Euclidean algorithm

---
+++ euclidean.py
@@ -0,0 +1,25 @@
+# Euclidean algorithm for computing greatest common divisor.
+
+def gcd(a, b):
+
+    if b >= a:
+        a, b = b, a
+
+    r0 = a % b
+    if r0 == 0:
+        return b
+
+    r1 = b % r0
+    if r1 == 0:
+        return r0
+
+    if r0 > 0 and r1 > 0:
+        rk_2 = r0
+        rk_1 = r1
+        rk = rk_2 % rk_1
+        while rk > 0:
+            rk_2 = rk_1
+            rk_1 = rk
+            rk = rk_2 % rk_1
+        else:
+            return rk_1
